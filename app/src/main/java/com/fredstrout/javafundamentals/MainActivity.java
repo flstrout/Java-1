7d520840c68dc8a96bc8cf47f6998485853b6a2f
flstrout <FredStrout617@fullsail.edu>
2015-09-04 01:33:18 -0500

    Alert Dialogs Working

--- app/src/main/java/com/fredstrout/javafundamentals/MainActivity.java
+++ app/src/main/java/com/fredstrout/javafundamentals/MainActivity.java
@@ -1,14 +1,14 @@
 package com.fredstrout.javafundamentals;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -50,10 +50,49 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             EditText intIndex = (EditText) findViewById(R.id.int_Index);
             int intIndex2 = Integer.parseInt(intIndex.getText().toString());
-            String retrievedData = strDataSet.get(intIndex2);
+            int dataSetSize = strDataSet.size();
 
-            Log.i(TAG, "Retrieved: " + retrievedData);
-            intIndex.setText("");
+            if (dataSetSize == 0) {
+
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(MainActivity.this);
+                builder2.setTitle("Houston, We Have A Problem!");
+                builder2.setMessage("The dataset is empty! Please input data and retry.");
+                builder2.setCancelable(true);
+                builder2.setPositiveButton("OK", null);
+
+                AlertDialog alertDialog2 = builder2.create();
+                alertDialog2.show();
+                intIndex.setText("");
+
+            }else if (dataSetSize > intIndex2) {
+
+                String retrievedData = strDataSet.get(intIndex2);
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                builder1.setTitle("The value for index " + intIndex2 + " is:");
+                builder1.setMessage(retrievedData);
+                builder1.setCancelable(true);
+                builder1.setPositiveButton("OK", null);
+
+                AlertDialog alertDialog1 = builder1.create();
+                alertDialog1.show();
+
+                Log.i(TAG, "Retrieved: " + retrievedData);
+                intIndex.setText("");
+
+            } else {
+
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(MainActivity.this);
+                builder2.setTitle("Houston, We Have A Problem!");
+                builder2.setMessage("The index you entered exceeds the number of records in the dataset. Please enter a number between 0 and " + (dataSetSize - 1) + ".");
+                builder2.setCancelable(true);
+                builder2.setPositiveButton("OK", null);
+
+                AlertDialog alertDialog2 = builder2.create();
+                alertDialog2.show();
+                intIndex.setText("");
+
+            }
 
         }
 
