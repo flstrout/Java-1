7be949acf548be73d36390a052da18b08c6540e8
flstrout <FredStrout617@fullsail.edu>
2015-09-04 00:32:12 -0500

    Data Collection Working

--- app/src/main/java/com/fredstrout/javafundamentals/MainActivity.java
+++ app/src/main/java/com/fredstrout/javafundamentals/MainActivity.java
@@ -2,15 +2,20 @@ package com.fredstrout.javafundamentals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    //LinkedHashSet<Editable> strDataSet = new LinkedHashSet<Editable>();
+    ArrayList<String> strDataSet= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             EditText txtData = (EditText) findViewById(R.id.txt_Data);
-            Log.i(TAG, "Data Submited: " + txtData.getText());
+
+            strDataSet.add(txtData.getText().toString());
+            for (int n = 0; n < strDataSet.size(); n++) {
+                Object obj = strDataSet.get(n);
+                Log.i(TAG, "Data Object = " + obj);
+            }
+            Log.i(TAG, "Data Set: " + strDataSet);
             txtData.setText("");
         }
     };
@@ -38,8 +49,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             EditText intIndex = (EditText) findViewById(R.id.int_Index);
-            Log.i(TAG, "Index Retrieved: " + intIndex.getText());
+            int intIndex2 = Integer.parseInt(intIndex.getText().toString());
+            String retrievedData = strDataSet.get(intIndex2);
+
+            Log.i(TAG, "Retrieved: " + retrievedData);
             intIndex.setText("");
+
         }
+
     };
 }
